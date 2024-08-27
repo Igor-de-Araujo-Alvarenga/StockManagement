23e39167be1679467a7185a46aafbab29daa9119
Igor Araujo <69015212+Igor-de-Araujo-Alvarenga@users.noreply.github.com>
2024-08-27 07:58:21 -0300

    Update function_history.sql

--- Schema/function_history.sql
+++ Schema/function_history.sql
@@ -11,3 +11,17 @@ CREATE TRIGGER after_insert_tb_products
 AFTER INSERT ON tb_products
 FOR EACH ROW
 EXECUTE FUNCTION log_product_insert();
+
+CREATE OR REPLACE FUNCTION log_product_update()
+RETURNS TRIGGER AS $$
+BEGIN
+    INSERT INTO tb_history (product_id, operation_id, created_at)
+    VALUES (NEW.id, 2, NOW());
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER after_update_tb_products
+AFTER UPDATE ON tb_products
+FOR EACH ROW
+EXECUTE FUNCTION log_product_update();
